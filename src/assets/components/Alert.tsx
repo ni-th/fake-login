ef01c5af9287ac9bc00c109b2c55004c14c1c2fb
nimantha1999 <nimantha.bt@gmail.com>
2026-01-13 17:12:02 +0530

    Fix formatting in Alert component

--- src/assets/components/Alert.tsx
+++ src/assets/components/Alert.tsx
@@ -3,6 +3,7 @@ const Alert = () => {
     <div className='fixed top-0 inset-0 flex items-center justify-center bg-red-700 z-10'>
         <h2 className='text-4xl font-bold'>This is not Facebook</h2>
     </div>
+    
   )
 }
 
